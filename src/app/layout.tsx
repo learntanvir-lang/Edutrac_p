47b73f5cb06404f3e62175d3c6ef18dc8b8ad067
Tanvir Mahmud <learntanvir@gmail.com>
2025-10-05 10:30:06 +0000

    make it the favicon (_for element <EduTrackLogo>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/icons/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
